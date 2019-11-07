c2ed61cbadf49010b309ad2062e3ad38ddc4ed59
Patrick Köstner <meliodas@Patricks-MacBook-Pro.local>
2019-11-07 19:35:21 +0100

    i dont know tbh

--- pyrobot/modules/Dice.py
+++ pyrobot/modules/Dice.py
@@ -9,7 +9,6 @@ def dice(bot: BOT, message: Message):
     max = int(message.command[1])
     dice=randint(1,max)
     message.delete()
-    #dice=str(wurf)
     max=str(max)
     dice=str(dice)
     bot.send_message(message.chat.id, "Deine Zahl aus 1 und "+ max + " beträgt: " + dice)
