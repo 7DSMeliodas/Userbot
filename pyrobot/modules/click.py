29622f4d4a52ae0326d2c2e6fa25b632b57324f2
Patrick Köstner <meliodas@Patricks-MacBook-Pro.local>
2019-11-07 19:33:57 +0100

    renamed and deleted some stuff

--- pyrobot/modules/click.py
+++
@@ -1,9 +0,0 @@
-from pyrobot import BOT
-from pyrogram import Filters, Message
-
-@BOT.on_message(Filters.command(["click"], ".") & Filters.me)
-def clicking(bot: BOT, message: Message):
-    if len(message.command) > 1:
-        message.reply_to_message.click(int(message.command[1]) - 1)
-    else:
-        message.reply_to_message.click(0)
--- pyrobot/modules/joinbeta.py
+++
@@ -1,9 +0,0 @@
-from pyrobot import BOT
-from pyrogram import Filters, Message
-
-@BOT.on_message(Filters.command("joinbeta", ".") & Filters.me)
-def joining(bot: BOT, message: Message):
-    result = message.reply_to_message.click(0)
-    message.delete()
-    code = result.replace("https://t.me/werewolfbetabot?start=", "")
-    bot.send_message(message.reply_to_message.from_user.id, "/start " + code)
