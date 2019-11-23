018bd375a92f8e32b40f953988bec2e02560f71f
Webspacer1 <57862243+Webspacer1@users.noreply.github.com>
2019-11-23 15:31:00 +0100

    Update liveclock.py

--- pyrobot/modules/liveclock.py
+++ pyrobot/modules/liveclock.py
@@ -20,7 +20,29 @@ def delete(bot: BOT, message: Message):
 
     while i < endtime:
         currenttime = time.localtime()
-        clock = time.strftime("My current localtime: %d/%m/%Y, %H:%M:%S", currenttime)
+        clock = time.strftime("Current local time: %H:%M:%S", currenttime)
+        bot.edit_message_text(message.chat.id, message.message_id, clock)
+        i=i+1
+        time.sleep(1)
+
+@BOT.on_message(Filters.command("timedate", ".") & Filters.me)
+def delete(bot: BOT, message: Message):
+
+    if not len(message.command) > 1:
+        message.edit("<b>Wrong input!</b>\nCorrect Syntax:\n.timedate 20\nShow the current time for 20 seconds")
+        sleep(3)
+        message.delete()
+        return
+
+    i=0
+
+    if len(message.command) > 1 and int(message.command[1]):
+        endtimestr = message.command[1]
+        endtime = int(endtimestr)
+
+    while i < endtime:
+        currenttime = time.localtime()
+        clock = time.strftime("Current local time/date:\n %H:%M:%S, %d.%m.%Y", currenttime)
         bot.edit_message_text(message.chat.id, message.message_id, clock)
         i=i+1
         time.sleep(1)
