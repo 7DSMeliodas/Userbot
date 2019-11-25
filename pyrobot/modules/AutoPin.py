d9ab49a3e7b507cc67869879fdb9c664287dacea
Meliodas <navycis@me.com>
2019-11-25 10:19:53 +0100

    Update AutoPin.py

--- pyrobot/modules/AutoPin.py
+++ pyrobot/modules/AutoPin.py
@@ -2,7 +2,7 @@ from pyrobot import BOT
 from pyrogram import Filters, Message
 
 #PinSilent
-@BOT.on_message(Filters.regex("^#players: (\d+)") & Filters.chat(['Ethiowerewolf', '-1001280664204']) & ~Filters.edited)
+@BOT.on_message(Filters.regex("^#players: (\d+)") & Filters.chat(['Ethiowerewolf', -1001280664204]) & ~Filters.edited)
 def pin_on_start_Ethiowolf_Silent(bot: BOT,  message:Message):
     toPin = message.message_id
     bot.pin_chat_message(message.chat.id, toPin, disable_notification=True)
